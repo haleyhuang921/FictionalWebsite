10aa66451817200afff1a85f5ae3e1eea2fec20c
haleyhuang921 <haley.h24@k12.xrds.org>
2024-01-23 14:25:34 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/confirmation')
+def confirmation():
+    name = request.args.get('name')
+    email = request.args.get('email')
+    props = {
+        "name": name,
+        "email": email
+    }
+    return render_template("confirmation.html", data= props)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
